fbfd2d5c452b41c1eae98a4e72915ff4a7d28951
Zoe <dadac123@gmail.com>
2016-08-26 16:15:29 +0800

    add random list to break_time.py

--- _src/stage3/break_time.py
+++ _src/stage3/break_time.py
@@ -1,11 +1,17 @@
 import time
 import webbrowser
+import random
 
 total_breaks = 3
 break_count = 0
 
+web_list = ["http://www.google.com", "http://twitter.com", 
+"http://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+
+
 print("This program started on "+time.ctime())
 while(break_count < total_breaks):
     time.sleep(5)
-    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    web = random.choice(web_list)
+    webbrowser.open(web)
     break_count = break_count + 1
