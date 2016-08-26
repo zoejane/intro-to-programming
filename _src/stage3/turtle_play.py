0c806bed78b182d4b8336e8a69bbcfed1abdbdf0
Zoe <dadac123@gmail.com>
2016-08-26 16:56:44 +0800

    improving code quality - use loop and function

--- _src/stage3/turtle_play.py
+++ _src/stage3/turtle_play.py
@@ -1,23 +1,21 @@
 import turtle
+def draw_square(some_turtle):
+	for i in range(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
 
-def draw_square():
+def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
+	# Create the turtle Brad - Draws a square
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("yellow")
 	brad.speed(2)
+	draw_square(brad)
 
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-
+	# Create the turtle Angie - Draws a circle
 	angie = turtle.Turtle()
 	angie.shape("arrow")
 	angie.color("blue")
@@ -25,4 +23,4 @@ def draw_square():
 
 	window.exitonclick()
 
-draw_square()
+draw_art()
