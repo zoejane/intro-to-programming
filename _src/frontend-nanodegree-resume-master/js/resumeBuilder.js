94dac1493c81d6b750585f9b471515ec979d1f06
Zoe <dadac123@gmail.com>
2016-10-04 17:37:39 +0800

    add navigator bar

--- _src/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ _src/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -193,3 +193,13 @@ function inName(oldName) {
 $("#main").append(internationalizeButton);
 
 $("#mapDiv").append(googleMap);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
