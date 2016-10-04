4f1bbaa5535aef70eb4c24347d108aa5f4b62888
Zoe <dadac123@gmail.com>
2016-10-04 15:37:05 +0800

    remove Indentation,add comments and semicolon

--- _src/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ _src/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,21 +1,20 @@
 var bio = {
-	"name": "Zoe Jane",
-	"role": "Learner",
-	"welcomeMessage": "Hello world!",
-	"biopic": "images/me.png",
-	"contacts": {
-		"mobile":"123456",
-		"email": "dadac123@gmail.com",
-		"github": "zoejane",
-		"twitter": "@dadac123",
-		"blog": "http://zoejane.net/",
-		"location":  "China"
-	},
-	"skills": ["programming","writing","music"]
-}
+ 	"name": "Zoe Jane",
+ 	"role": "Learner",
+ 	"welcomeMessage": "Hello world!",
+ 	"biopic": "images/me.png",
+ 	"contacts": {
+ 		"mobile":"123456",
+ 		"email": "dadac123@gmail.com",
+ 		"github": "zoejane",
+ 		"twitter": "@dadac123",
+ 		"blog": "http://zoejane.net/",
+ 		"location":  "China"
+ 	},
+ 	"skills": ["programming","writing","music"]
+ };
 
 bio.display = function(){
-	// use prepend to make sure name and role showed at beginning, before "contacts" part.
 	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 	$("#header").prepend(formattedRole);
 	var formattedName = HTMLheaderName.replace("%data%",bio.name);
@@ -28,7 +27,6 @@ bio.display = function(){
 	$("#topContacts").append(HTMLblog.replace("%data%",bio.contacts.blog));
 	$("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
 
-	// footerContacts is at the bottom of the page, under "Let's Connect" part.
 	$("#footerContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
 	$("#footerContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
 	$("#footerContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
@@ -50,28 +48,28 @@ bio.display = function(){
 		formattedSkill = HTMLskills.replace("%data%",bio.skills[2]);
 		$("#skills").append(formattedSkill);
 	 }
-}
+};
 bio.display();
 
 var education = {
-	"schools": [
-	{
-		"name": "CSUST",
-		"location": "Changsha, China",
-		"degree":"Master",
-		"dates": "2013",
-		"major":["GIS","Finance"]
-	}
-	],
-	"onlineCourses": [
-	{
-		"title":"Intro to Programming",
-		"school":"Udacity",
-		"dates":"2016",
-		"url": "http://www.udacity.com"
-	}
-	]
-}
+ 	"schools": [
+ 	{
+ 		"name": "CSUST",
+ 		"location": "Changsha, China",
+ 		"degree":"Master",
+ 		"dates": "2013",
+ 		"major":["GIS","Finance"]
+ 	}
+ 	],
+ 	"onlineCourses": [
+ 	{
+ 		"title":"Intro to Programming",
+ 		"school":"Udacity",
+ 		"dates":"2016",
+ 		"url": "http://www.udacity.com"
+ 	}
+ 	]
+ };
 
 education.display = function(){
 	for (school in education.schools){
@@ -84,7 +82,6 @@ education.display = function(){
 		$(".education-entry:last").append(HTMLschoolMajor.replace("%data%",education.schools[school].major));
 	}
 
-	// add onlineCourses
 	if (education.onlineCourses.length > 0 ){
 		$("#education").append(HTMLonlineClasses);
 	}
@@ -97,78 +94,82 @@ education.display = function(){
 		$(".education-entry:last").append(HTMLonlineURL.replace("%data%",education.onlineCourses[online].url));
 	}
 	
-}	
+};	
 education.display();
 
 var work = {
-	"jobs": [
-	{
-		"employer": "Udacity",
-		"title": "Course Developer",
-		"location": "Mountain View, CA",
-		"dates": "Feb 2014 - Current",
-		"description": "Who moved my cheese cheesy feet cauliflower cheese."
-	},
-	{
-		"employer": "LearnBIG",
-		"title": "Software Engineer",
-		"location": "Seattle, WA",
-		"dates": "May 2013 - Jan 2014",
-		"description": "Who moved my cheese cheesy feet cauliflower cheese."
-	},
-	{
-		"employer": "LEAD Academy Charter High School",
-		"title": "Science Teacher",
-		"location": "Nashville, TN",
-		"dates": "Jul 2012 - May 2013",
-		"description": "Who moved my cheese cheesy feet cauliflower cheese."
-	},
-	{
-		"employer": "Stratford High School",
-		"title": "Science Teacher",
-		"location": "Nashville, TN",
-		"dates": "Jun 2009 - Jun 2012",
-		"description": "Who moved my cheese cheesy feet cauliflower cheese."
-	}
-	]
+  "jobs": [
+    {
+      "employer": "Udacity",
+      "title": "Course Developer",
+      "location": "Mountain View, CA",
+      "dates": "Feb 2014 - Current",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LearnBIG",
+      "title": "Software Engineer",
+      "location": "Seattle, WA",
+      "dates": "May 2013 - Jan 2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LEAD Academy Charter High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jul 2012 - May 2013",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Stratford High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jun 2009 - Jun 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
 };
 
 work.display = function(){
-	for(job in work.jobs){
-		$("#workExperience").append(HTMLworkStart);
+for(job in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
 
-		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
-		var formattedEmployerTitle = formattedEmployer + formattedTitle;
-		$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-		var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
-		var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
-		var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
-		$(".work-entry:last").append(formattedLocation);
-		$(".work-entry:last").append(formattedDates);
-		$(".work-entry:last").append(formattedDescription);
-	}
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+	var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+	var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+	var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+
+	$(".work-entry:last").append(formattedLocation);
+	$(".work-entry:last").append(formattedDates);
+	$(".work-entry:last").append(formattedDescription);
 }
+
+};
 
 work.display();
 
 var project = {
-	"projects": [
-	{
-		"title": "Movie Website",
-		"dates":"2016",
-		"description": "Using Python and HTML/CSS",
-		"images": ["images/project1.jpg","images/project1.jpg"]
-	},
-	{
-		"title": "Resume",
-		"dates": "2016",
-		"description": "Using JavaScript",
-		"images": ["images/project2.jpg"]
-	}
-	]
-}
+ 	"projects": [
+ 	{
+ 		"title": "Movie Website",
+ 		"dates":"2016",
+ 		"description": "Using Python and HTML/CSS",
+ 		"images": ["images/project1.jpg","images/project1.jpg"]
+ 	},
+ 	{
+ 		"title": "Resume",
+ 		"dates": "2016",
+ 		"description": "Using JavaScript",
+ 		"images": ["images/project2.jpg"]
+ 	}
+ 	]
+
+ };
 
 projects.display = function(){
 	for (project_index in project.projects){
@@ -189,19 +190,20 @@ projects.display = function(){
 			}
 		}
 	}
-}
+};
+
 projects.display();
 
-// code for internationalizeButton
 var name = bio.name;
 function inName(oldName) {
     var finalName = oldName;
     position = finalName.search(' ')
     finalName = finalName[0].toUpperCase()
-    + finalName.slice(1,position).toLowerCase()
-    + finalName.slice(position).toUpperCase()
+    +finalName.slice(1,position).toLowerCase()
+    +finalName.slice(position).toUpperCase()
     return finalName;
-};
+}
+
 $("#main").append(internationalizeButton);
 
 $("#mapDiv").append(googleMap);
